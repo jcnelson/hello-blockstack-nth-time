743b4fe19716059fc97fdb0d8c1def340344c1d9
Jude Nelson <judecn@gmail.com>
2017-06-22 20:00:17 -0400

    sync with new API

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   document.getElementById('signin-button').addEventListener('click', function() {
- 
+
+    // ephemeral private key
     var privkey_hex = '0be113bd33b0c6d0c7167d3ebf3752531a1358dd44b5883a88d11335f8e13ca3';
-    var device_id = "5a726553-237c-4d96-b5e4-bc143b5c1014";
     var domain_name = window.location.origin;
     var manifest_url = domain_name + "/manifest.json";
     var redirect_url = domain_name + "/login";
-    var authRequest = blockstack.makeAuthRequest(privkey_hex, domain_name, manifest_url, redirect_url, []);
+    var authRequest = blockstack.makeAuthRequest(privkey_hex, domain_name, manifest_url, redirect_url, ['store_write']);
 
     blockstack.redirectUserToSignIn(authRequest)
   })
@@ -19,74 +19,54 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('error-message').innerHTML = errorMessage
   }
 
-  function getLoginCount(privkey_hex, session_token, device_id) {
-    // get or create datastore...
-    return blockstack_storage.datastoreConnectOrCreate("localhost:6270", ['disk'], privkey_hex, session_token, device_id, [device_id]).then(
-    (datastore_ctx) => {
+  function getLoginCount() {
+    // get the counts file...
+    return blockstack_storage.getFile("/counts.txt")
+    .then((count_txt) => {
 
-       // get the counts file...
-       return blockstack_storage.getFile(datastore_ctx, "/counts.txt").then(
-       (count_txt) => {
+       var count = 0;
+       if( count_txt.error && count_txt.errno == 2 ) {
+          // does not exist
+          count = 1;
+       }
+       else if( count_txt.error ) {
+          // some other error 
+          showError("Failed to get /counts.txt: " + count_txt.error);
+       }
+       else if( count_txt ) {
+          // exists!
+          count = parseInt(count_txt);
+          if( !count ) {
+             throw new Error("Failed to parse " + count_txt);
+          }
+       }
+       else {
+          throw new Error("Unknown count value");
+       }
 
-          var count = 0;
-          if( count_txt.error && count_txt.errno == 2 ) {
-             // does not exist
-             count = 1;
-          }
-          else if( count_txt.error ) {
-             // some other error 
-             showError("Failed to get /counts.txt: " + count_txt.error);
-          }
-          else if( count_txt ) {
-             // exists!
-             count = parseInt(count_txt);
-             if( !count ) {
-                throw new Error("Failed to parse " + count_txt);
-             }
-          }
-          else {
-             throw new Error("Unknown count value");
-          }
-
-          var new_counts_txt = (count + 1) + "";
-          
-          // update the counts file...
-          return blockstack_storage.putFile(datastore_ctx, "/counts.txt", new_counts_txt).then(
-             (success) => {
-                showError("");
-                return count + 1;
-             },
-             (error) => {
-                showError("Failed to update /counts.txt: " + error);
-                return {'error': 'Failed to update /counts.txt: ' + error};
-             });
-
+       var new_counts_txt = (count + 1) + "";
+       
+       // update the counts file...
+       return blockstack_storage.putFile("/counts.txt", new_counts_txt)
+       .then((success) => {
+          showError("");
+          return count + 1;
        },
        (error) => {
-          showError("Failed to get /counts.txt: " + error);
-          return {'error': 'Failed to get /counts.txt'};
+          showError("Failed to update /counts.txt: " + error);
+          return {'error': 'Failed to update /counts.txt: ' + error};
        });
     },
     (error) => {
-       showError("Failed to connect to datastore: " + error);
+       showError("Failed to get /counts.txt: " + error);
        return {'error': 'Failed to get /counts.txt'};
     });
   }
 
   function showProfile(profile) {
     // load the person's datastore 
-    
-    var privkey_hex = '0be113bd33b0c6d0c7167d3ebf3752531a1358dd44b5883a88d11335f8e13ca3';
-    var device_id = "5a726553-237c-4d96-b5e4-bc143b5c1014";
-    var authResponse = blockstack.getAuthResponseToken();
-    authResponse = blockstack.decodeToken(authResponse);
-    console.log(authResponse);
-
-    var session_token = authResponse.payload.core_token;
-    console.log(session_token);
-
-    getLoginCount(privkey_hex, session_token, device_id).then(
-    (count) => {
+    getLoginCount()
+    .then((count) => {
        var person = new blockstack.Person(profile)
        document.getElementById('heading-name').innerHTML = person.name() + ' (login count: ' + JSON.stringify(count) + ')'
        document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
